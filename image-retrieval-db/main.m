c64a2f092a3410df7fe4fc4372807139d6523b30
Zander <zander.hamidi@gmail.com>
2019-10-24 17:47:13 -0500

    Delete main.m

--- image-retrieval-db/main.m
+++
@@ -1,7 +0,0 @@
-%% Alexander Hamidi, adhamid2
-%  ECE 417 Spring 2016
-%  Mark Hasegawa Johnson
-%  Mary Pietrowicz
-
-% replace directory with path to data folder (the one from the website)
-run('/Users/Zander/Desktop/finalRelease');
